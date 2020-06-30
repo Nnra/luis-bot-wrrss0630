7b700fd95e4f6f1feb39c8f3b4db84cd93d4b241
Nura <nura721@gmail.com>
2020-06-30 17:56:27 +0800

    add template

---
+++ publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild CoreBot.sln -p:DeployOnBuild=true -p:PublishProfile=.\luis-bot-wrrss0630-Web-Deploy.pubxml -p:Password=Es1gWWsM8Dlbp6nr5a7g40fLmwesidWfKFJaDY5MSYnEoNnnB1t6WoTcuiXp
+
